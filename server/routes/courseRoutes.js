eeb537006d3f2fdf258aa7fd62e7275cdc200797
Harshrj53 <hrsj31@gmail.com>
2026-04-23 14:06:32 +0530

    fix: restore missing express import in courseRoutes

--- server/routes/courseRoutes.js
+++ server/routes/courseRoutes.js
@@ -1,3 +1,4 @@
+import express from 'express';
 import { getCourses, getCourseById, getProgress, markCompleted } from '../controllers/courseController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
